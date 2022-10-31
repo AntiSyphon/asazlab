39d32e9185251082f9c743e76346664fa55b897d
kickler@blackhillsinfosec.com <kickler@blackhillsinfosec.com>
2022-10-31 03:17:19 -0600

    av

--- 1-AD/scripts/1-vmbootstrap/8-disable-defender.ps1
+++ 1-AD/scripts/1-vmbootstrap/8-disable-defender.ps1
@@ -4,6 +4,7 @@ cd c:\ASAZ
 Add-MpPreference -ExclusionPath C:\ASAZ
 Set-MpPreference -DisableRealtimeMonitoring $true
 New-ItemProperty -Path “HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender” -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
+Uninstall-WindowsFeature -Name Windows-Defender –whatif
 IEX(New-Object Net.Webclient).DownloadString('https://github.com/AntiSyphon/asazlab/raw/main/1-AD/scripts/1-vmbootstrap/9-disable-defender.ps1') 
 
 
