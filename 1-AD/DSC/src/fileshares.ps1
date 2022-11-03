6c4ac632e04e8de76a6a35142fc660dd13156640
kickler@blackhillsinfosec.com <kickler@blackhillsinfosec.com>
2022-11-03 01:42:18 -0600

    remove dns dsc from fs

--- 1-AD/DSC/src/fileshares.ps1
+++ 1-AD/DSC/src/fileshares.ps1
@@ -12,7 +12,7 @@ configuration fileshares {
 
     ) 
     
-    Import-DscResource -ModuleName ActiveDirectoryDsc, NetworkingDsc, xPSDesiredStateConfiguration, xDnsServer, ComputerManagementDsc
+    Import-DscResource -ModuleName ActiveDirectoryDsc, NetworkingDsc, xPSDesiredStateConfiguration, ComputerManagementDsc
     
     [String] $DomainNetbiosName = (Get-NetBIOSName -DomainFQDN $DomainFQDN)
     [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
