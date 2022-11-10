ff9d0933a41e23663f22124820c435ded86de970
kickler@blackhillsinfosec.com <kickler@blackhillsinfosec.com>
2022-11-09 21:38:47 -0700

    cleanup c2 script

--- 2-C2/scripts/Install-C2s.sh
+++ 2-C2/scripts/Install-C2s.sh
@@ -1,46 +1,31 @@
 #!/bin/bash
 
-# This script is used for building a tooled up Debian install
-# Includes some apt installs, some git clonesand pip
-# Script available for wide distribution
-
-
 # Run as root, please, it's just easier that way
 sudo -s
-
 
 # housekeeping
 apt update
 apt upgrade -y
-
 
 # Install python3.9
 mkdir /opt/install-logs/
 apt install python3.9 -y | tee -a /opt/install-logs/python3.9.log
 apt update |tee -a /opt/install-logs/apt-update-after-python.log
 
-
 # Use virtual environments to containerize python-based tooling
 apt install python3.9-dev python3.9-venv -y | tee -a /opt/install-logs/python-devs.log
-
 
 # pip installer for 3.9
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py | tee -a /opt/install-logs/pip.log
 
-
 # install a pre-req for a cffi package req
-# broke here, everything after is sus
 apt install libffi-dev -y | tee -a /opt/install-logs/libffi.log
-
 
 # Add nmap whois zip
 apt install nmap net-tools whois zip -y
 
-
-# First up: impacket
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-
+# impacket install and setup
 cd /opt/
 git clone https://github.com/SecureAuthCorp/impacket.git
 cd impacket
@@ -52,10 +37,7 @@ python3.9 -m pip install .
 deactivate
 cd /opt/
 
-
-# Next up: CrackMapExec
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-
+# CrackMapExec install and setup
 cd /opt/
 git clone https://github.com/DefensiveOrigins/APT22Things.git
 mv APT22Things CrackMap
@@ -68,10 +50,7 @@ python3.9 cme
 deactivate
 cd /opt/
 
-
-# PlumHound
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-
+# PlumHound install
 cd /opt/ 
 git clone https://github.com/PlumHound/PlumHound.git
 cd PlumHound
@@ -82,10 +61,7 @@ python3.9 -m pip install -r requirements.txt
 deactivate
 cd /opt/
 
-
 # BloodHound.py
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-
 cd /opt/
 git clone https://github.com/fox-it/BloodHound.py.git
 cd BloodHound.py
@@ -95,37 +71,6 @@ python3.9 -m pip install wheel
 python3.9 setup.py install
 deactivate
 cd /opt/
-
-
-# BruteLoops
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-
-cd /opt/
-git clone https://github.com/DefensiveOrigins/BruteLoops.git
-cd BruteLoops
-python3.9 -m venv bl-env
-source bl-env/bin/activate
-python3.9 -m pip install wheel
-python3.9 -m pip install -r requirements.txt
-deactivate
-cd /opt/
-
-
-# bl-bfg install (replace BruteLoops)
-# couple additional steps here, but seemed ok in testing
-
-cd /opt/
-git clone https://github.com/DefensiveOrigins/bl-bfg.git
-cd bl-bfg
-python3.9 -m venv bfg-env
-source bfg-env/bin/activate
-python3.9 -m pip install wheel
-python3.9 -m pip install .
-python3.9 setup.py install
-python3.9 -m pip install IPython
-deactivate
-cd /opt/
-
 
 # neo4j install
 echo "deb http://httpredir.debian.org/debian stretch-backports main" | sudo tee -a /etc/apt/sources.list.d/stretch-backports.list
@@ -151,7 +96,6 @@ curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/t
 chmod 755 msfinstall
 ./msfinstall |tee -a msf-install.log
 
-
 # silenttrinity
 cd /opt/
 git clone https://github.com/DefensiveOrigins/SILENTTRINITY.git
@@ -161,7 +105,6 @@ source st-env/bin/activate
 python3.9 -m pip install wheel
 python3.9 -m pip install -r requirements.txt
 deactivate
-
 
 # john the password ripper
 mkdir -p ~/src
@@ -173,7 +116,6 @@ cd ~/src/john/src
 ./configure && make -s clean && make -sj4
 mkdir /opt/john
 cp ~/src/john/run/ -r /opt/john/
-
 
 # setup location hashes
 mkdir /opt/hashes
