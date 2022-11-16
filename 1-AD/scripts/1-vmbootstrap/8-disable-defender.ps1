733db2c4d30dc7f8e15def86e5787ae8713857e6
kickler@blackhillsinfosec.com <kickler@blackhillsinfosec.com>
2022-11-16 00:08:18 -0700

    setup for multiclass

--- 1-AD/scripts/1-vmbootstrap/8-disable-defender.ps1
+++
@@ -1,9 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-mkdir c:\ASAZ
-cd c:\ASAZ
-Add-MpPreference -ExclusionPath C:\ASAZ
-IEX(New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/AntiSyphon/asazlab/main/1-AD/scripts/1-vmbootstrap/9-disable-defender%20.ps1') 
-                                              
-
-
-
